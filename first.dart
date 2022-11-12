f0703f17e0a4f3799f55501da3bf0751c4d3d395
simo1231211 <102697841+simo1231211@users.noreply.github.com>
2022-11-12 13:30:58 +0300

    Add files via upload

---
+++ first.dart
@@ -0,0 +1,24 @@
+import 'package:first/first.dart' as first;
+import "dart:io";
+
+void main() {
+  /* int num1;
+  print("Enter your number");
+
+  num1 = stdin.readByteSync();
+  if (num1 > 0) {
+    print("positiv");
+  } 
+   else {
+    print("nigative");
+  }
+  */
+  var w = 0;
+  if (w < 0) {
+    print("N");
+  } else if (w == 0) {
+    print("zero");
+  } else {
+    print("P");
+  }
+}
